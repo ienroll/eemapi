37bb486ff3bc44a1d5cd922801abad6ffedeee9f
rreddy14 <ravindar.vangala@gmail.com>
2019-11-25 01:12:07 -0600

    Handle Json date format

--- src/main/java/com/optum/eem/demo/config/JacksonConfig.java
+++ src/main/java/com/optum/eem/demo/config/JacksonConfig.java
@@ -1,10 +1,14 @@
 package com.optum.eem.demo.config;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -12,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 public class JacksonConfig {
@@ -19,13 +24,12 @@ public class JacksonConfig {
   private static final String dateFormat = "yyyy-MM-dd";
   private static final String dateTimeFormat = "yyyy-MM-dd HH:mm:ss";
 
-
   @Bean
-  @Primary
   public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
     ObjectMapper objectMapper = builder.build();
-    objectMapper.registerModule(new JavaTimeModule());
-    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    objectMapper.setSerializationInclusion(Include.NON_NULL); //delete fields with null values
+    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
     return objectMapper;
   }
 
@@ -36,9 +40,34 @@ public class JacksonConfig {
       havingValue = "none")
   public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
     return builder -> {
+      builder.modules(new JavaTimeModule());
       builder.simpleDateFormat(dateTimeFormat);
       builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
       builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
     };
+  }
+
+  @Bean
+  public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
+    MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+    converter.setObjectMapper(objectMapper());
+    return converter;
+  }
+
+  @Bean
+  @Primary
+  public ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    JavaTimeModule javaTimeModule = new JavaTimeModule();
+    javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+    javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(dateFormat)));
+
+    objectMapper.registerModule(javaTimeModule);
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    objectMapper.setSerializationInclusion(Include.NON_NULL); //delete fields with null values
+    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+    return objectMapper;
   }
 }
