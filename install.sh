5a37afc7d16f435142cc657f63441d108e13ddaf
rreddy14 <ravindar.vangala@gmail.com>
2019-12-05 14:31:28 -0600

    dockerize app

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ docker-compose -f node-exporter/docker-compose.yaml up -d
 docker-compose -f alertmanager/docker-compose.yaml up -d
 docker-compose -f grafana/docker-compose.yaml up -d
 docker-compose -f jaeger/docker-compose.yaml up -d
+docker-compose up -d
