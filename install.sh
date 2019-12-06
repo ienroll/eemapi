fce83317b515da18fec387a1fc3528ee31d939d2
rreddy14 <ravindar.vangala@gmail.com>
2019-12-06 11:52:45 -0600

    update install

--- install.sh
+++ install.sh
@@ -1,6 +1,1 @@
-docker-compose -f prometheus/docker-compose.yaml up -d
-docker-compose -f node-exporter/docker-compose.yaml up -d
-docker-compose -f alertmanager/docker-compose.yaml up -d
-docker-compose -f grafana/docker-compose.yaml up -d
-docker-compose -f jaeger/docker-compose.yaml up -d
 docker-compose up -d
