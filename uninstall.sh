1e785b4319fb641b1d68722f37f1aadbd3365e9c
rreddy14 <ravindar.vangala@gmail.com>
2019-12-06 11:53:34 -0600

    combine all images into single compose file!

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,1 @@
-docker stop eemapi
-docker stop alertmanager
-docker stop grafana
-docker stop node-exporter
-docker stop jaeger
-docker stop prometheus
-
-docker rm eemapi
-docker rm alertmanager
-docker rm grafana
-docker rm node-exporter
-docker rm jaeger
-docker rm prometheus
+docker-compose down
