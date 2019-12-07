723c54266aa81ec2f45a57df519b1ca87d6fa42b
rreddy14 <ravindar.vangala@gmail.com>
2019-12-07 13:59:11 -0600

    ip address generation

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-export HOST_IP=192.168.1.113
+export HOST_IP=`ifconfig en0 |awk '/inet / {print $2; }'`
+echo "HOST_IP=$HOST_IP"
 find . -name '*.yml' -print0 | xargs -0 sed -i "" "s/{{HOST_IP}}/${HOST_IP}/g"
+
 docker-compose up -d
